2b5ea5b70f3b6b9e9ffb5f26bb6b5d6706484d4e
yvar192 <ivar.scholten@protonmail.com>
2019-10-07 21:52:42 +0200

    remove unused scripts

--- .scripts/LaunchPolybar.sh
+++
@@ -1,3 +0,0 @@
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
-done
--- .scripts/volume.py
+++
@@ -1,6 +0,0 @@
-from os import system
-from time import sleep
-
-while True:
-    system("/usr/share/i3blocks/volume 5 pulse")
-    sleep(0.1)
