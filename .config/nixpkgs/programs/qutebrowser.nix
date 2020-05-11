ca2a90a979463587dfaf74378912a3969f431a03
Ivar <ivar.scholten@protonmail.com>
2020-05-11 23:25:49 +0200

    removed shit that used to fix an issue for youtube but isnt needed anymore

--- .config/nixpkgs/programs/qutebrowser.nix
+++ .config/nixpkgs/programs/qutebrowser.nix
@@ -106,9 +106,4 @@
       };
     };
   };
-
-  # Note that indentation is fucked, qutebrowser doesn't accept it any other way so now its backwards.
-  extraConfig = " 
-config.set('content.headers.user_agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/77.0', 'https://accounts.google.com/*')\n 
-  ";
 }
