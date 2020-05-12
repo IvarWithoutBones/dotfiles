a9ec074b4ba250704198077d5200d2be66e746ea
Ivar <ivar.scholten@protonmail.com>
2020-05-12 02:47:03 +0200

    system config: cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,5 @@
 { config, pkgs, ... }:
 {
-  # Include the results of the hardware scan. You can generate this with nixos-generate-config.
   imports = [
     ./hardware-configuration.nix
   ];
@@ -8,63 +7,56 @@
   # links /libexec from derivations to /run/current-system/sw
   environment.pathsToLink = [ "/libexec" ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Networking options.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
   };
 
-  # Console properties.
   console = {
     keyMap = "us";
     font = "Lat2-Terminus16";
   };
 
-  # Set the time zone to Amsterdam.
-  time.timeZone = "Europe/Amsterdam";
-
-  # Allow unfree packages.
+  # Allow unfree packages. Required for the propietary Nvidia driver
   nixpkgs.config.allowUnfree = true;
 
-  # System packages.
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-  ];
+  time.timeZone = "Europe/Amsterdam";
 
-  # Always update the Linux packages to their latest versions.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Configure services
   services = {
-    openssh.enable = true; # Enable the OpenSSH daemon.
+    openssh.enable = true;
     xserver = {
       enable = true;
       layout = "us";
       xkbOptions = "eurosign:e";
       videoDrivers = [ "nvidiaBeta" ];
+      digimend.enable = true;
       displayManager.lightdm.enable = true;
       windowManager.i3 = {
-      	package = pkgs.i3-gaps;
-      	enable = true;
-	extraPackages = with pkgs; [
+        package = pkgs.i3-gaps;
+        enable = true;
+        extraPackages = with pkgs; [
 	  i3blocks
-	  i3status
 	  i3lock
-	  dmenu
+          dmenu
 	];
       };
     };
   };
 
-  # Enable sound.
+  # Required for some GTK based app's settings to be saved
+  programs.dconf.enable = true;
+
   sound.enable = true;
 
-  # Configure hardware options. Note that 32-bit support is enabled as some Steam games require this.
+  # 32-bit support is enabled as some Steam games require this
   hardware = {
     opengl.enable = true;
     opengl.driSupport32Bit = true;
@@ -72,16 +64,18 @@
     pulseaudio.support32Bit = true;
   };
 
-  # Define user accounts.
   users.users = {
     ivar = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ and network managing permissions for the user.
+      extraGroups = [ "wheel" "networkmanager" ];
       shell = pkgs.zsh;
     };
   };
 
-  nix.allowedUsers = [ "@wheel" ];
+  nix = {
+    allowedUsers = [ "@wheel" ]; # Allow users in the "wheel" group to control the nix deamon
+    autoOptimiseStore = true;
+  };
 
-  system.stateVersion = "20.03"; # Do not change this unless told to. 
+  system.stateVersion = "20.09";
 }
