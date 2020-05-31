47ed9e363f8b300ae8415248d13e5df6c2348744
IvarWithoutBones <ivar.scholten@gmail.com>
2020-05-31 20:32:31 +0200

    update-script: remove useless command

--- .scripts/update-system.sh
+++ .scripts/update-system.sh
@@ -8,7 +8,6 @@ function do_but_green {
 do_but_green "nix-channel --update"
 do_but_green "home-manager switch --keep-going"
 do_but_green "sudo nixos-rebuild switch --upgrade"
-do_but_green "sudo nixos-rebuild boot --upgrade"
 do_but_green "nix-collect-garbage"
 
 if [ "$1" == "-f" ]; then # In case you really wanna save some space
