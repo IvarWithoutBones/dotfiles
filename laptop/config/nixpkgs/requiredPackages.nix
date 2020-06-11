764a7edf1e93e4008180fcb621860d5723ef75b8
Ivar <ivar.scholten@protonmail.com>
2020-06-11 21:59:10 +0200

    requiredPackages: move to common

--- laptop/config/nixpkgs/requiredPackages.nix
+++ laptop/config/nixpkgs/requiredPackages.nix
@@ -1,18 +1,1 @@
-globalConfig: let
-  pkgs = globalConfig.pkgs;
-in
-with pkgs; [ # These packages are required by my dotfiles
-  st
-  dmenu
-  sysstat
-  imagemagick
-  perl
-  i3lock
-  maim # Required for lock script & screenshots
-  dconf # Required for some GTK based app's settings to be saved
-  
-  (python38.withPackages (pkgs: with pkgs; [
-    setuptools
-    dbus-python
-  ]))
-]
+../../../common/config/nixpkgs/requiredPackages.nix
