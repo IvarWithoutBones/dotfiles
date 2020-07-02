d35a096a8929177a98523b33f45e5b32ec6ca0fc
IvarWithoutBones <ivar.scholten@protonmail.com>
2020-07-02 22:13:55 +0200

    use wifi driver version from nixpkgs, as this finally got updated

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -1,16 +1,4 @@
 { config, pkgs, ... }:
-let
-  # Required till https://github.com/NixOS/nixpkgs/pull/88809 is merged, version currently in nixpkgs does not compile with more recent kernels.
-  wifi = (config.boot.kernelPackages.rtl8821ce.overrideAttrs (attrs: {
-    version = "5.5.2_34066.20200325";
-    src = pkgs.fetchFromGitHub {
-      owner = "tomaspinho";
-      repo = "rtl8821ce";
-      rev = "69765eb288a8dfad3b055b906760b53e02ab1dea";
-      sha256 = "17jiw25k74kv5lnvgycvj2g1n06hbrpjz6p4znk4a62g136rhn4s";
-    };
-  }));
-in
 {
   imports = [ ./hardware-configuration.nix ];
 
@@ -23,7 +11,7 @@ in
       efi.canTouchEfiVariables = true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
-    extraModulePackages = [ wifi ];
+    extraModulePackages = with config.boot.kernelPackages; [ rtl8821ce ];
     kernelParams = [ "acpi_backlight=vendor" ];
   };
 
