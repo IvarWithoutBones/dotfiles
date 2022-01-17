a460b274c03bf139b67ef0793c92d3f96f51e4d0
Ivar Scholten <ivar.scholten@protonmail.com>
2022-01-17 21:36:28 +0100

    flake: support setting the nixos system from a profile

--- flake.nix
+++ flake.nix
@@ -12,12 +12,70 @@
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, flake-utils }: {
-    nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = inputs @ { self, nixpkgs, home-manager, flake-utils }: rec {
+    lib = rec {
+      inherit (nixpkgs) lib;
+
+      configFromProfile = profile: {
+        nixpkgs.overlays = [];
+      } // profile;
+
+      nixosConfigFromProfile = profile:
+        { system, extraModules ? [], extraConfig ? {}, ... }:
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = extraModules
+              ++ [( extraConfig )]
+              ++ (configFromProfile profile).modules;
+          };
+    };
+
+    # TODO: create a seperate file for this, we also want to optionally integrate home-manager. preferably from nixos module
+    # This also means we have to properly seperate everything into their own module, so that each profile can import whatever they choose.
+    testProfile = {
       modules = [
         ./configuration.nix
-        ({ pkgs, config, ... }: {
+      ];
+    };
+
+    nixosConfigurations = {
+      nixos-pc = lib.nixosConfigFromProfile testProfile {
+        system = "x86_64-linux";
+
+        # TODO: remove the { ... } part when nvidia shit is factored out.
+        extraConfig = { config, ...}: {
+          networking.hostName = "nixos-pc";
+
+          services.xserver = {
+            videoDrivers = [ "nvidia" ];
+            screenSection = '' # Fixes screentearing on nvidia GPUs
+              Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
+            '';
+          };
+
+          hardware = {
+            nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+            cpu.intel.updateMicrocode = true;
+          };
+        };
+
+        extraModules = [
+          home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = {
+              hasBattery = false;
+              sm64Rom = /mnt/hdd/roms/n64/baserom.us.z64;
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ivv = (import ./home-manager/home.nix) inputs;
+          }
+        ];
+      };
+
+      nixos-laptop = lib.nixosConfigFromProfile testProfile {
+        system = "x86_64-linux";
+
+        extraConfig = { config, ... }: {
           networking.hostName = "nixos-laptop";
 
           boot = {
@@ -37,52 +95,24 @@
             };
           };
           hardware.cpu.amd.updateMicrocode = true;
-        })
-        home-manager.nixosModules.home-manager {
-          home-manager.extraSpecialArgs = {
-            hasBattery = true;
-            sm64Rom = /home/ivv/misc/roms/n64/sm64.z64;
-          };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ivv = (import ./home-manager/home.nix) inputs;
-        }
-      ];
-    };
-    nixosConfigurations.nixos-pc = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        ({ pkgs, config, ... }: {
+        };
 
-          networking.hostName = "nixos-pc";
-
-          services.xserver = {
-            videoDrivers = [ "nvidia" ];
-            screenSection = '' # Fixes screentearing on nvidia GPUs
-              Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
-          '';
-          };
-          hardware = {
-            # TODO: currently `beta` fails to compile, switch this back as soon as it doesn't
-            nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
-            cpu.intel.updateMicrocode = true;
-          };
-        })
-        home-manager.nixosModules.home-manager {
-          home-manager.extraSpecialArgs = {
-            hasBattery = false;
-            sm64Rom = /mnt/hdd/roms/n64/baserom.us.z64;
-          };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ivv = (import ./home-manager/home.nix) inputs;
-        }
-      ];
+        extraModules = [
+          home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = {
+              hasBattery = true;
+              sm64Rom = /home/ivv/misc/roms/n64/sm64.z64;
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ivv = (import ./home-manager/home.nix) inputs;
+          }
+        ];
+      };
     };
   } // (flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
-  in {
+ in {
     devShell = pkgs.mkShell {
       buildInputs = with pkgs; [
         coreutils
