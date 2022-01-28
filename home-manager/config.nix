e70b9c5bbbcaedd96cbc5dc98bcbc3a8810ee3f3
IvarWithoutBones <ivar.scholten@protonmail.com>
2022-01-28 01:18:25 +0100

    home-modules: cleanup

--- home-manager/config.nix
+++ home-manager/config.nix
@@ -1,9 +1,3 @@
 {
   allowUnfree = true;
-  fonts.fontconfig.defaultFonts.emoji = "Twitter Color Emoji";
-  packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
 }
--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,28 +1,29 @@
 { pkgs, config, ... }:
-let
-  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; };
-in {
+
+{
   home.packages = with pkgs; [
-    arc-theme capitaine-cursors arc-icon-theme
-    twitter-color-emoji
+    arc-theme
+    arc-icon-theme
+    capitaine-cursors
     
     # General utils
-    alacritty
-    htop
-    ripgrep
     wget git
+    ripgrep
+    htop
     unar
-    neofetch
     tree
     bat
-    feh
     python3
     file
+    feh
   
+    # Graphical utils
+    alacritty
+    pavucontrol
+
     # Nix specific utils
     nix-index
     nix-prefetch-git
-    cachix
     comma
   
     # Media
@@ -33,15 +34,10 @@ in {
     krita
     _1password-gui
     discord
-    pavucontrol
     transmission-gtk
   
     # Emulators/games
-    dolphinEmuMaster
     citra
-    lutris # for cemu
-    #nur.repos.ivar.ryujinx
-    #nur.repos.ivar.yuzu-ea
     steam
     minecraft
   ]; 
