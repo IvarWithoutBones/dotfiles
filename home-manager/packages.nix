d1a50cf57d72daf0a393c78410ac53ba32d81eff
Ivar Scholten <ivar.scholten@protonmail.com>
2022-01-31 20:17:41 +0100

    user-packages: add update-system

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,6 +1,38 @@
 { pkgs, config, ... }:
 
-{
+let
+  update-system = pkgs.writeShellScriptBin "update-system" ''
+    set -e
+
+    DOTFILES_DIR="${config.home.homeDirectory}/nix/dotfiles"
+
+    while getopts ":lgh" arg; do
+      case $arg in
+        l)
+          DONT_UPDATE=1 ;;
+        g)
+          DONT_COLLECT_GARBAGE=1 ;;
+        h)
+          printf "Usage: "$(basename "$0")" [-lg]\n  [-l] Don't update flake\n  [-g] Don't collect garbage\n" && exit 0 ;;
+      esac
+    done
+
+    runColored() {
+      printf "\e[32m\$ "%s"\n\e[0m" "$1"
+      $1
+    }
+
+    if [ -z "$DONT_UPDATE" ]; then
+      pushd "$DOTFILES_DIR" 1>/dev/null
+      runColored "nix flake update"
+      popd 1>/dev/null
+    fi
+
+    runColored "sudo nixos-rebuild switch --impure"
+
+    [[ -z "$DONT_COLLECT_GARBAGE" ]] && runColored "nix-collect-garbage"
+  '';
+in {
   home.packages = with pkgs; [
     arc-theme
     arc-icon-theme
@@ -25,6 +57,7 @@
     nix-index
     nix-prefetch-git
     comma
+    update-system
   
     # Media
     ncspot spotify
