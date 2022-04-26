8e658c60d5edf328863593df06e754004b78f1e5
= <ivar.scholten@protonmail.com>
2022-04-26 15:51:09 +0200

    nvim: remove depricated nvim-tree option

--- home-manager/modules/nvim.nix
+++ home-manager/modules/nvim.nix
@@ -53,8 +53,6 @@
 
           lua << EOF
             require'nvim-tree'.setup {
-              auto_close = true,
-
               update_focused_file = {
                 enable      = true,
                 update_cwd  = true,
