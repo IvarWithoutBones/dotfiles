121c2e9303b8f9f0971b38edb1619d4a389a0ea8
Ivar Scholten <ivar.scholten@protonmail.com>
2022-08-17 23:02:28 +0200

    config.nix: manage declaratively

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -3,10 +3,19 @@
 }:
 
 {
+  # Applies inside of the home-manager config
   nixpkgs.config = {
     allowUnfree = true;
     experimental-features = "nix-command flakes";
   };
+
+  # Applies for imperative commands
+  xdg.configFile."nixpkgs/config.nix".text = ''
+    {
+      allowUnfree = true;
+      experimental-features = "nix-command flakes";
+    }
+  '';
 
   programs = {
     command-not-found.enable = true;
