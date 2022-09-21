25c021c8fb0ed63bac14c4abfccc43cb97c3dc19
Ivar Scholten <ivar.scholten@protonmail.com>
2022-09-21 22:49:13 +0200

    nixpkgs-pr: cleanup

--- pkgs/nixpkgs-pr/nixpkgs-pr.sh
+++ pkgs/nixpkgs-pr/nixpkgs-pr.sh
@@ -1,9 +1,8 @@
-#! /usr/bin/env bash
+# Automatically open a PR to nixpkgs, and ping maintainers of the modified package.
 
-# Automatically open a PR to nixpkgs for the current branch, and ping maintainers of the modified package.
+set -euo pipefail
 
-set -e
-
+# TODO: make this configurable
 GITHUB_NAME="IvarWithoutBones"
 
 error() {
@@ -11,160 +10,155 @@ error() {
     exit 1
 }
 
-while (( "$#" > 0 )); do
-    case "$1" in
-        -n|new|--new)
-            NEW_PKG=1 ;;
-        -o|old|--old)
-            UPDATED_PKG=1 ;;
-        -h|help|--help)
-            echo "Usage: $(basename "$0") [-n] [-o] attribute"
-            echo "  -n: new package"
-            echo "  -o: updated package"
-            echo "  -h: show this message"
-            exit 0 ;;
-        *)
-            ATTR="$1" ;;
-    esac
-    shift 1
-done
-
-if [[ ! $OLD_PKG && ! $UPDATED_PKG ]]; then
-    UPDATED_PKG=1
-elif [[ $OLD_PKG && $UPDATED_PKG ]]; then
-    error "Options -n and -o are mutually exclusive!"
-fi
-
-if [ -z "${ATTR}" ]; then
-    error "No attribute was specified!"
-fi
-
-NIXPKGS_ROOT="$(git rev-parse --show-toplevel 2>/dev/null)" || error "not in a git tree!"
-if [ ! -f "${NIXPKGS_ROOT}/default.nix" ]; then
-    error "could not locate nixpkgs default.nix"
-fi
-
-getMetaAttr() {
-    jq -r ".$@" <<< "${PKG_META}"
-}
-
-getName() {
-    nix eval --impure --raw --expr "
-      let
-        lib = (import ${NIXPKGS_ROOT} { }).lib;
-      in lib.getName \"$@\"
-    "
-}
-
-getPackage() {
-    PKG_META="$(nix eval -f "${NIXPKGS_ROOT}" --json "$1".meta)"
-
-    PKG_NAME="$(getName "$(getMetaAttr "name")")"
-    PKG_DESCRIPTION="$(getMetaAttr "description")"
-    PKG_HOMEPAGE="$(getMetaAttr "homepage")"
-    PKG_CHANGELOG="$(getMetaAttr "changelog")"
-    PKG_MAINTAINERS=($(getMetaAttr "maintainers[] | .github"))
-}
-
 pingMaintainers() {
-    DESCRIPTION=""
+    local -r maintainers=($@)
+    local text maintainerList
 
-    if (( "${#PKG_MAINTAINERS[@]}" > 0 )); then
-        MAINTAINERS=""
-
-        for maintainer in "${PKG_MAINTAINERS[@]}"; do
-            [[ "${maintainer}" = "$GITHUB_NAME" ]] && continue
-
-            MAINTAINERS+=" @${maintainer}"
+    if (( "${#maintainers[@]}" > 0 )); then
+        for maintainer in "${maintainers[@]}"; do
+            [[ "$maintainer" == "$GITHUB_NAME" ]] && continue
+            maintainerList+=" @$maintainer"
         done
-    
-        if [ "${MAINTAINERS-}" ]; then
-            DESCRIPTION+="cc${MAINTAINERS}"
+
+        if [ "${maintainerList-}" ]; then
+            text+="cc${maintainerList}"
         fi
     fi
 
-    echo "${DESCRIPTION}"
+    echo "${text-}"
 }
 
-newPkgDescription() {
-    DESCRIPTION="This adds [${PKG_NAME}](${PKG_HOMEPAGE}), "
-    FIRST_WORD="${PKG_DESCRIPTION%% *}"
+newPackageDescription() {
+    local -r name="$1" meta="$2"
+    local -r description="$(jq -r ".description" <<< "$meta")"
+    local -r homepage="$(jq -r ".homepage" <<< "$meta")"
+    local text="This adds [$name]($homepage), "
 
-    case "${FIRST_WORD,,}" in
+    # Make sure the beginning of the sentence makes sense
+    local -r firstWord="${description%% *}"
+    case "${firstWord,,}" in
         the|an|a)
             ;;
         *)
-            DESCRIPTION+="an " ;;
+            text+="an " ;;
     esac
 
-    if [[ "${FIRST_WORD}" =~ ^[[:upper:]]+$ && "${#FIRST_WORD}" -gt 1 ]]; then
+    if [[ "$firstWord" =~ ^[[:upper:]]+$ ]] && (( "${#firstWord}" > 1 )); then
         # If the entire word is capitalised use it as is
-        DESCRIPTION+="${PKG_DESCRIPTION}."
+        text+="$description."
     else
         # Alternatively remove capitalisation
-        DESCRIPTION+="${PKG_DESCRIPTION,}."
+        text+="${description,}."
     fi
 
-    if [ "$(pingMaintainers)" ]; then
-        DESCRIPTION+="\n\n$(pingMaintainers)"
-    fi
-
-    echo "${DESCRIPTION}"
-}
-
-updatedPkgDescription() {
-    DESCRIPTION=""
-
-    if [[ ! "${PKG_CHANGELOG}" = "null" ]]; then
-        DESCRIPTION+="See [the changelog](${PKG_CHANGELOG}) for more information."
-        shouldAddNewlines=1
-    fi
-
-    if [ "$(pingMaintainers)" ]; then
-        [ "${shouldAddNewlines}" ] && DESCRIPTION+="\n\n"
-        DESCRIPTION+="$(pingMaintainers)"
-    fi
-
-    echo "${DESCRIPTION}"
+    echo "${text-}"
 }
 
 generatePullRequestTemplate() {
-    tmpdir="$(mktemp -d /tmp/nixpkgs-pr.XXX)"
-    trap "rm -rf ${tmpdir}" EXIT
+    local -r nixpkgsRoot="$1" attr="$2" operation="$3"
+    local -r tmpDir="$(mktemp -td "nixpkgs-pr.XXXXXX")"
+    local -r meta="$(nix --extra-experimental-features nix-command eval -f "$nixpkgsRoot" --json "$attr".meta)"
+    local prDescription shouldAddNewlines
 
-    echo "Fetching pull request template..."
-    curl https://raw.githubusercontent.com/NixOS/nixpkgs/master/.github/PULL_REQUEST_TEMPLATE.md -o "${tmpdir}/pr-template.md"
+    # Fetch the upstream pull request template and remove the comments
+    curl -ns "https://raw.githubusercontent.com/NixOS/nixpkgs/master/.github/PULL_REQUEST_TEMPLATE.md" -o "$tmpDir/pr-template.md"
+    pandoc --wrap=preserve --tab-stop 2 --to gfm --strip-comments "$tmpDir/pr-template.md" --output "$tmpDir/patched-pr-template.md"
 
-    # Remove comments
-    pandoc --wrap=preserve --tab-stop 2 --to gfm --strip-comments "${tmpdir}/pr-template.md" --output "${tmpdir}/patched-pr-template.md"
+    # Generate the actual description for the PR
+    if [[ "$operation" == "new" ]]; then
+        # We want to use `lib.getName` instead of `pname` for compatibility, not all derivations provide `pname`.
+        local name="$(nix --extra-experimental-features nix-command eval -f "$nixpkgsRoot" --raw "$attr".name)"
+        name="$(nix --extra-experimental-features nix-command eval --impure --raw --expr "
+            (import $nixpkgsRoot { }).lib.getName \"$name\"
+        ")"
 
-    # TODO: figure out target automatically
-    if [ "${NEW_PKG}" ]; then
-        TARGET_DESCRIPTION="$(newPkgDescription)"
-    elif [ "${UPDATED_PKG}" ]; then
-        TARGET_DESCRIPTION="$(updatedPkgDescription)"
+        prDescription+="$(newPackageDescription "$name" "$meta")"
+        shouldAddNewlines=1
+    elif [[ "$operation" == "old" ]]; then
+        local changelog="$(jq -r ".changelog" <<< "$meta")"
+        # Add a link to the changelog, if present
+        if [[ "${changelog}" != "null" ]]; then
+            prDescription+="See [the changelog]($changelog) for more information."
+            shouldAddNewlines=1
+        fi
+    fi
+
+    # Ping the maintainers of the package
+    local -r maintainers="$(pingMaintainers $(jq -r ".maintainers[] | .github" <<< "$meta"))"
+    if [ -n "$maintainers" ]; then
+        # Insert newlines if we already generated any text prior to this
+        [ -n "${shouldAddNewlines-}" ] && prDescription+="\n\n"
+        prDescription+="$maintainers"
     fi
 
     # Insert our generated description
-    if [ "${TARGET_DESCRIPTION}" ]; then
-        sed -ie "/^###### Description of changes/a ${TARGET_DESCRIPTION}" "${tmpdir}/patched-pr-template.md"
+    if [ -n "$prDescription-" ]; then
+        sed -ie "/^###### Description of changes/a $prDescription" "$tmpDir/patched-pr-template.md"
     fi
 
     # Check "Fits CONTRIBUTING.md" automatically, i wouldn't open a PR otherwise
-    sed -ie "s/\[ \] Fits \[CONTRIBUTING.md\]/\[X\] Fits \[CONTRIBUTING.md\]/g" "${tmpdir}/patched-pr-template.md"
-
-    echo "Succesfully patched the description"
+    sed -ie "s/\[ \] Fits \[CONTRIBUTING.md\]/\[X\] Fits \[CONTRIBUTING.md\]/g" "$tmpDir/patched-pr-template.md"
+    echo "$tmpDir/patched-pr-template.md"
 }
 
-encodeAndOpenUrl() {
-    GIT_BRANCH="$(git branch --show-current)"
-    BODY="$(cat "${tmpdir}/patched-pr-template.md" | python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read()))")"
-    URL="https://github.com/NixOS/nixpkgs/compare/master...${GITHUB_NAME}:${GIT_BRANCH}?quick_pull=1&body=${BODY}"
-    
-    echo "Launching browser..."
-    xdg-open "$URL" &>/dev/null
+encodeUrl() {
+    local -r template="$1"
+    local gitBranch body url
+    gitBranch="$(git branch --show-current)"
+    body="$(cat "$template" | python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read()))")"
+    echo "https://github.com/NixOS/nixpkgs/compare/master...${GITHUB_NAME}:$gitBranch?quick_pull=1&body=$body"
 }
 
-getPackage "$ATTR"
-generatePullRequestTemplate
-encodeAndOpenUrl
+main() {
+    local operation attr nixpkgsRoot
+
+    # Parse shell arguments
+    while (( "$#" > 0 )); do
+        case "$1" in
+            -n|new|--new)
+                test -n "${operation-}" && error "options -n and -o are mutually exclusive!"
+                operation="new"
+                attr="$2"
+                shift 2
+                ;;
+            -o|old|--old)
+                test -n "${operation-}" && error "options -n and -o are mutually exclusive!"
+                operation="old"
+                attr="$2"
+                shift 2
+                ;;
+            -h|help|--help)
+                echo "Usage: $(basename "$0") [-h,help] [-n,new] [-o,old] <attribute>"
+                echo "    new           generate a description for a new package"
+                echo "    old           generate a description for a existing package"
+                echo "    attribute     the attribute path of the package to generate a description for"
+                echo "    help          show this message"
+                exit
+                ;;
+            *)
+                if [ -z "${attr-}" ]; then
+                    attr="$1"
+                    shift
+                else
+                    error "unknown argument $1"
+                fi
+                ;;
+        esac
+    done
+
+    test -z "${attr-}" && error "No attribute was specified!"
+    local -r nixpkgsRoot="$(git rev-parse --show-toplevel 2>/dev/null)" || error "not in a git tree!"
+    test -f "$nixpkgsRoot/default.nix" || error "could not locate nixpkgs root"
+
+    # Generate the PR body with a description based on the modified package
+    echo "fetching pull request template"
+    template="$(generatePullRequestTemplate "$nixpkgsRoot" "$attr" "${operation:-old}")"
+    trap 'rm -rf "$(dirname "$template")"' EXIT INT TERM
+    echo "succesfully fetched the template and generated a description"
+
+    # Finally open the browser with the generated description
+    echo "opening the pull request page in your browser"
+    xdg-open "$(encodeUrl "$template")" &>/dev/null
+}
+
+main $@
