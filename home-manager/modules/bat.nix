e06faccef45802011cf49ed2e8fb60be34dce795
Ivar Scholten <ivar.scholten@protonmail.com>
2022-10-25 22:37:05 +0200

    bat: enable mouse scrolling, dont use builtins.readFile

--- home-manager/modules/bat.nix
+++ home-manager/modules/bat.nix
@@ -4,23 +4,32 @@
 , ...
 }:
 
+let
+  # Needed for mouse/gesture scrolling support
+  pager = "${pkgs.less}/bin/less --mouse --raw-control-chars --wheel-lines=1 --quit-if-one-screen";
+in
 {
   programs.bat = {
     enable = true;
-
-    themes = {
-      catpuccin = builtins.readFile (pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/catppuccin/sublime-text/3d8625d937d89869476e94bc100192aa220ce44a/Mocha.tmTheme";
-        sha256 = "sha256-D2qufwRF72MvESoYsvOlniBr2zir1y2unPBQ+7Q+AT4=";
-      });
+    config = {
+      theme = "catpuccin";
+      inherit pager;
     };
-
-    config.theme = "catpuccin";
   };
 
-  # A cache needs to be rebuild for the themes to show up
+  # The themes option from the module needs builtins.readFile :/
+  xdg.configFile."bat/themes/catpuccin.tmTheme".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/sublime-text/3d8625d937d89869476e94bc100192aa220ce44a/Mocha.tmTheme";
+    sha256 = "sha256-D2qufwRF72MvESoYsvOlniBr2zir1y2unPBQ+7Q+AT4=";
+  };
+
+  # Cache needs to be rebuild for the themes to show up
   home.activation.bat = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.bat}/bin/bat cache --build 1>/dev/null
+    $DRY_RUN_CMD ${pkgs.bat}/bin/bat cache --build 1>/dev/null
+  '';
+
+  # Export this so that programs such as delta can use it
+  programs.zsh.initExtra = lib.optionalString config.programs.zsh.enable ''
+    export BAT_PAGER='${pager}'
   '';
 }
-
