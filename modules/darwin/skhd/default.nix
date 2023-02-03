a52ce3fd444d7b4abe281eb865374afe91304d4f
Ivar Scholten <ivar.scholten@protonmail.com>
2023-02-03 16:03:41 +0100

    yabai: add shortcut to move space between window

--- modules/darwin/skhd/default.nix
+++ modules/darwin/skhd/default.nix
@@ -6,6 +6,7 @@
 
 let
   yabai = "${pkgs.yabai}/bin/yabai";
+  jq = "${pkgs.jq}/bin/jq";
   spaces = [ 1 2 3 4 5 6 7 8 9 10 ];
 
   # Shorthand to generate shortcut attributes for all spaces
@@ -18,14 +19,25 @@ let
     spaces);
 
   # Switch the focus between windows in both stack and bsp modes
-  navigateWindow = stack: bsp: pkgs.writeShellScript "yabai-navigate" ''
-    layout="$(${yabai} -m query --spaces | ${pkgs.jq}/bin/jq -r '.[] | select(."has-focus" == true) | .type')"
+  navigateWindow = stack: bsp: pkgs.writeShellScript "yabai-navigate.sh" ''
+    layout="$(${yabai} -m query --spaces | ${jq} -r '.[] | select(."has-focus" == true) | .type')"
 
     if [[ "$layout" = "stack" ]]; then
       ${yabai} -m window --layer ${stack}
     elif [[ "$layout" = "bsp" ]]; then
       ${yabai} -m window --focus ${bsp}
     fi
+  '';
+
+  # Move the currently focused space to another display
+  moveSpaceBetweenWindows = pkgs.writeShellScript "move-space-between-windows.sh" ''
+    currentDisplay="$(${yabai} -m query --spaces | ${jq} -r '.[] | select(."has-focus" == true) | .display')"
+    otherDisplay="$(${yabai} -m query --displays \
+      | ${jq} -r --argjson space "$currentDisplay" '.[] | select(.index != $space) | .index')"
+
+    ${yabai} -m space --create # Ensure there is at least one available space
+    ${yabai} -m space --display "$otherDisplay"
+    ${yabai} -m display --focus "$otherDisplay"
   '';
 in
 {
@@ -44,6 +56,7 @@ in
 
       # Focus a different monitor
       "alt - n" = "${yabai} -m display --focus recent";
+      "alt + shift - n" = moveSpaceBetweenWindows;
 
       # Change the window layout
       "ctrl + shift + alt - h" = "${yabai} -m window --warp west";
