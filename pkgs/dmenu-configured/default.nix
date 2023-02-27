f2d252088b2507f031f3c52e3a878d1323fd3e20
Ivar Scholten <ivar.scholten@protonmail.com>
2023-02-27 21:54:49 +0100

    dmenu: add fuzzy matching

--- pkgs/dmenu-configured/default.nix
+++ pkgs/dmenu-configured/default.nix
@@ -1,27 +1,47 @@
-{ runCommand
-, dmenu
+{ lib
+, stdenvNoCC
 , fetchpatch
 , makeWrapper
+, dmenu
 }:
 
-# A wrapped version of dmenu using the same theme used by i3/sway, using with the same height as the bar
-runCommand "dmenu-configured"
-{
-  _dmenu = dmenu.override {
+let
+  dmenuWithPatches = dmenu.override {
     patches = [
-      # Allow configuring the height of the window with "-h"
+      # Fuzzy matching
+      (fetchpatch {
+        url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
+        sha256 = "sha256-zfmsKfN791z6pyv+gA6trdfKvNnCCULazVtk1sibDgA=";
+      })
+
+      # Allow configuring the theme with Xresources
+      (fetchpatch {
+        url = "https://tools.suckless.org/dmenu/patches/xresources/dmenu-xresources-4.9.diff";
+        sha256 = "sha256-Np9I8hhnwmGA3W5v4tSrBN9Or8Q2Ag9x8H3yf8L9jDI=";
+      })
+
+      # Allow configuring the height of the statusline
       (fetchpatch {
         url = "https://tools.suckless.org/dmenu/patches/line-height/dmenu-lineheight-5.2.diff";
         sha256 = "sha256-QdY2T/hvFuQb4NAK7yfBgBrz7Ii7O7QmUv0BvVOdf00=";
       })
     ];
   };
-  nativeBuildInputs = [ makeWrapper ];
-} ''
-  mkdir -p $out/bin
-  for bin in $_dmenu/bin/*; do
-    # TODO: inherit these colors somehow
-    makeWrapper $bin $out/bin/$(basename ''${bin}) \
-      --add-flags "-nf '#cdd6f4' -nb '#12121c' -sb '#cba6f7' -sf '#12121c' -h 23"
-  done
-''
+in
+dmenuWithPatches.overrideAttrs (prevAttrs: {
+  postPatch = prevAttrs.postPatch or "" + ''
+    # Patch in the desired statusline height
+    substituteInPlace config.def.h --replace \
+      'static unsigned int lineheight = 0;' \
+      'static unsigned int lineheight = 24;'
+  '';
+
+  passthru = {
+    xresources = ''
+      dmenu.background: #12121C
+      dmenu.foreground: #CDD6F4
+      dmenu.selbackground: #CBA6F7
+      dmenu.selforeground: #12121C
+    '';
+  } // prevAttrs.passthru or { };
+})
