f02cc0d3c28ea7bb6d96c540a1d90a26033bfd91
Ivar Scholten <ivar.scholten@protonmail.com>
2024-02-27 23:22:52 +0100

    nix-search-fzf: add support for keybindings to switch subcommands

--- pkgs/nix-search-fzf/nix-search-fzf.sh
+++ pkgs/nix-search-fzf/nix-search-fzf.sh
@@ -7,7 +7,7 @@ set -eou pipefail
 
 FLAKE="nixpkgs"         # The default flake to use. TODO: make this configurable
 NIX_SUBCOMMAND="shell"  # The default nix subcommand to execute
-MULTIPLE_SELECTION=true # Wether to allow the user to select multiple derivations
+MULTIPLE_SELECTION=true # Whether to allow the user to select multiple derivations
 PRINT_COMMAND=false     # Only print the command that would be executed, don't execute it
 
 if [ -n "${XDG_CACHE_HOME-}" ]; then
@@ -15,6 +15,11 @@ if [ -n "${XDG_CACHE_HOME-}" ]; then
 else
 	CACHE_PATH="$HOME/.cache/nix-search-fzf/cache.txt"
 fi
+
+# Because fzf executes commands from keybindings in a subprocess, we cannot directly change this scripts state.
+# Instead we can use a temporary file as an IPC mechanism, to change which subcommand to execute.
+TMP_FILE="$(mktemp --dry-run --suffix "-nix-search-fzf")"
+trap 'rm -f "$TMP_FILE"' EXIT INT TERM
 
 handleArguments() {
 	while (("$#" > 0)); do
@@ -69,22 +74,39 @@ manageCache() {
 	local doUpdate="${1:-false}"
 	mkdir -p "$(dirname "$CACHE_PATH")"
 
-	local cacheOutdated=false
-	if (($(date -r "$CACHE_PATH" +%s) < $(date -d "now - 10 days" +%s))); then
-		cacheOutdated=true
+	if [ ! -f "$CACHE_PATH" ]; then
+		doUpdate=true
+		echo "cache file does not exist, generating..." >&2
+	elif (($(date -r "$CACHE_PATH" +%s) < $(date -d "now - 10 days" +%s))); then
+		doUpdate=true
+		echo "cache file is older than 10 days, updating..." >&2
 	fi
 
-	if [ "$doUpdate" == "true" ] || [ "$cacheOutdated" == "true" ] || ! test -f "$CACHE_PATH"; then
-		if $cacheOutdated; then
-			echo "cache file is older than 10 days, updating"
-		fi
-
-		echo "caching attribute paths..."
+	if [ "$doUpdate" == "true" ]; then
+		echo "caching attribute paths..." >&2
 		# Create a list of all attribute paths with "legacyPackages.$arch" stripped
-		# In the future this could contain metadata as well, doing a "nix-eval" for each isnt the fastest
+		# In the future this could contain metadata as well, doing a "nix-eval" for each is not the fastest
 		nix search "$FLAKE" --json | jq -r 'keys[]' | cut -d'.' -f3- > "$CACHE_PATH"
-		echo "succesfully cached the attribute paths"
+		echo "successfully generated the cache" >&2
 	fi
+}
+
+fzfBindingFlag() {
+	local tmpFile="$1"
+	local -A bindings=(
+		["shell"]="ctrl-s"
+		["build"]="ctrl-b"
+		["edit"]="ctrl-e"
+		["run"]="ctrl-r"
+	)
+
+	local result="--bind="
+	for subCommand in "${!bindings[@]}"; do
+		local binding="${bindings[$subCommand]}"
+		# When pressed, write the appropriate command to our temporary IPC file, and change the prompt accordingly
+		result+="$binding:execute-silent(echo $subCommand > $tmpFile)+change-prompt($subCommand > ),"
+	done
+	echo "${result%,}"
 }
 
 runFzf() {
@@ -96,11 +118,12 @@ runFzf() {
 	fi
 
 	fzf "$multi_flag" \
-		--height=40% \
-		--prompt="$NIX_SUBCOMMAND > " \
-		--preview-window=right,70% \
+		--height 40% \
+		--preview-window right,70% \
 		--border rounded \
-		--preview "bash -c \"@previewText@ {} $FLAKE\"" < "$CACHE_PATH"
+		--prompt "$NIX_SUBCOMMAND > " \
+		--preview "bash -c \"@previewText@ {} $FLAKE\"" \
+		"$(fzfBindingFlag "$TMP_FILE")" < "$CACHE_PATH"
 }
 
 runNix() {
@@ -120,6 +143,9 @@ runNix() {
 	fi
 
 	((${#packages} == 0)) && exit 0
+
+	# Update what subcommand to execute, in case it was changed by a keybinding from fzf
+	[ -s "$TMP_FILE" ] && NIX_SUBCOMMAND="$(< "$TMP_FILE")"
 
 	command="NIXPKGS_ALLOW_UNFREE=1 nix $NIX_SUBCOMMAND $FLAKE#$packages --impure"
 	if [ "$PRINT_COMMAND" == true ]; then
