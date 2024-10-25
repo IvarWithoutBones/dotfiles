a6bd38eb0132ae84747585da6b6e692f2c30ba7d
Ivar Scholten <ivar.scholten@protonmail.com>
2024-10-25 15:31:47 +0200

    flake: update lock, darwin: simplify nixos-builder

--- modules/darwin/nixos-builder.nix
+++ modules/darwin/nixos-builder.nix
@@ -1,35 +1,13 @@
-{ pkgs
-, lib
-, ...
+{ ...
 }:
 
 # Configuration for a NixOS virtual machine on which Nix can perform builds if linux-only packages are needed.
-# TODO: Use the `nixos-builder` nix-darwin module.
 
-let
-  builderScript = pkgs.writeShellScriptBin "nixos-builder" ''
-    set -xeuo pipefail
-
-    NIXOS_BUILDER_DIRECTORY="''${NIXOS_BUILDER_DIR:-$HOME/nix/nixos-builder}"
-    mkdir -p "$NIXOS_BUILDER_DIRECTORY"
-    cd "$NIXOS_BUILDER_DIRECTORY"
-    ${pkgs.darwin.linux-builder}/bin/create-builder "''$*"
-  '';
-in
 {
   nix = {
-    distributedBuilds = true;
-
-    buildMachines = [{
-      hostName = "localhost";
-      sshUser = "builder";
-      system = "x86_64-linux";
-      maxJobs = 10;
-      # The script from `pkgs.darwin.builder` will create a matching keypair.
-      sshKey = "/etc/nix/builder_ed25519";
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
-    }];
+    linux-builder = {
+      enable = true;
+      ephemeral = true; # Do not retain state
+    };
   };
-
-  environment.systemPackages = lib.toList builderScript;
 }
