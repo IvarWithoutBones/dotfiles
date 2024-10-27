eef04d1e5390497ed05c764ce47f8de02288daf2
Ivar Scholten <ivar.scholten@protonmail.com>
2024-10-27 23:44:49 +0100

    helix: update config, add more language servers

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -1,24 +1,79 @@
-{ pkgs
+{ lib
+, pkgs
 , ...
 }:
 
+let
+  packages = with pkgs; [
+    taplo-lsp # TOML
+    nodePackages.vscode-json-languageserver # JSON
+    yaml-language-server # YAML
+    typescript-language-server # Typescript/Javascript
+    vscode-langservers-extracted # HTML/CSS
+    sumneko-lua-language-server # Lua
+    glsl_analyzer # GLSL
+    cmake-language-server # CMake
+
+    # C/C++
+    clang-tools
+    clang
+
+    # C#
+    dotnet-sdk
+    omnisharp-roslyn
+
+    # Bash
+    shellcheck
+    nodePackages.bash-language-server
+    shfmt
+
+    # Python
+    python3Packages.python-lsp-server # Python
+    ruff
+
+    # Nix
+    nil-language-server
+    nixpkgs-fmt
+
+    # Rust
+    cargo
+    rustfmt
+    rustc
+    clippy
+    rust-analyzer
+  ];
+
+  # A hacky way to add packages to helix's environment if they are not already present in $PATH.
+  # Needed to allow projects to overwrite tools we bundle with the editor, for example when we want to use nightly Rust for one project.
+  # Note that we cannot use `extraPackages` as the defaults would take priority over packages from the environment.
+  helixWithDefaultPackages = pkgs.runCommand "helix-with-default-packages"
+    {
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+    } ''
+    mkdir -p $out/bin
+    makeWrapper ${lib.getExe pkgs.helix} $out/bin/hx --suffix PATH : ${lib.makeBinPath packages}
+  '';
+in
 {
   programs.helix = {
     enable = true;
-
-    package = pkgs.buildEnv {
-      name = "helix-with-deps";
-      paths = with pkgs; [
-        helix
-        clippy
-      ];
-    };
+    package = helixWithDefaultPackages;
 
     settings = {
       theme = "catppuccin_mocha";
 
       keys.normal = {
-        C-f = ":format";
+        C-f = ":format"; # Format the current buffer
+        A-q = ":buffer-close"; # Close the current buffer
+
+        "A-]" = ":buffer-next"; # Jump to the next buffer
+        "A-[" = ":buffer-previous"; # Jump to the previous buffer
+
+        # Navigate between views using Alt+{h,j,k,l}
+        A-h = "jump_view_left";
+        A-j = "jump_view_down";
+        A-k = "jump_view_up";
+        A-l = "jump_view_right";
       };
 
       editor = {
@@ -48,12 +103,14 @@
           left = [
             "mode"
             "spacer"
+            "diagnostics"
             "spinner"
             "file-name"
-            "diagnostics"
           ];
 
           right = [
+            "version-control"
+            "separator"
             "workspace-diagnostics"
             "separator"
             "selections"
@@ -67,26 +124,32 @@
     };
 
     languages = {
-      nix = {
-        language-server.command = "${pkgs.nil-language-server}/bin/nil";
-        formatter.command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-        auto-format = false;
-      };
+      language = [
+        {
+          name = "nix";
+          formatter.command = "nixpkgs-fmt";
+        }
 
-      rust = {
-        language-server = {
-          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
-          config = {
-            checkOnSave.command = "clippy";
+        {
+          name = "bash";
+          formatter = {
+            command = "shfmt";
+            args = [
+              "--binary-next-line"
+              "--space-redirects"
+              "--case-indent"
+              "--simplify"
+              "--case-indent"
+              "--apply-ignore"
+              "-" # Use standard input/output
+            ];
           };
-        };
-      };
+        }
+      ];
 
-      bash = {
-        name = "bash";
-        language-server = {
-          command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
-          args = [ "start" ];
+      language-server = {
+        rust-analyzer = {
+          config.checkOnSave.command = "clippy";
         };
       };
     };
