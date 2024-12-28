d1ba4bca93eef3d1f4dfcc0359a9c629d1d2915f
Ivar Scholten <ivar.scholten@protonmail.com>
2024-12-28 16:20:12 +0100

    nix: remove unused substituters
    Checking if a derivation is present on a binary cache takes a while,
    having unused entries actively slows down building derivations locally.

--- modules/nix.nix
+++ modules/nix.nix
@@ -31,16 +31,6 @@ in
 
       # Can causes failures on Darwin, see https://github.com/NixOS/nix/issues/7273.
       auto-optimise-store = !pkgs.stdenvNoCC.isDarwin;
-
-      substituters = [
-        "https://ivar.cachix.org"
-        "https://ivar-personal.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "ivar.cachix.org-1:oPUMlRJ2cwtWP3mdNUBe1esfL3+kw5aSWnkseeOn92o="
-        "ivar-personal.cachix.org-1:xcf/K8QYcw2XR7Qz8QXNVVWxufSb6Lw5+rkh+CN4cTM="
-      ];
     };
   };
 }
