f78dc16728b89a621db44daa27cac38317a8b63a
Ivar Scholten <ivar.scholten@protonmail.com>
2025-02-13 22:27:29 +0100

    templates: add rust-shell

--- templates/default.nix
+++ templates/default.nix
@@ -18,7 +18,7 @@
       chmod +x ./scripts/* && mkdir crates && direnv allow
       ```
 
-      ## Configure the package name in `flake.nix` and create a new Cargo crate.
+      ## Configure the package name in `flake.nix` and create a new Cargo workspace.
       ```sh
       NAME="$(basename "$(readlink -f .)")"; sed -i "s/rust-app/''${NAME}/" ./flake.nix && cargo new --vcs none "./crates/''${NAME}" && cargo build
       ```
@@ -26,6 +26,27 @@
       ## Create a Git repository.
       ```sh
       git init && git add --all && git commit --message "initial commit"
+      ```
+    '';
+  };
+
+  rust-shell = {
+    path = ./rust-shell;
+    description = "A flake providing a bare-bones Rust/Cargo development shell.";
+    welcomeText = ''
+      # Things left to do manually:
+
+      ## Create a Git repository and enter a development shell.
+      ```sh
+      git init && git add --all && git commit --message "initial commit" && direnv allow
+      ```
+
+      ## When using this shell for an external repository that doesn't use Nix, add that repository as a submodule.
+      A flake has to be included in the Git index for Nix to recognize it, even though you might only want to use it locally.
+      Creating a new repository for the flake which includes the external repository as a submodule allows you to do that.
+
+      ```sh
+      git submodule add URL ./src
       ```
     '';
   };
---
+++ templates/rust-shell/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rustfmt" "rust-analyzer" "clippy" ];
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          rustToolchain
+        ];
+      };
+    });
+}
