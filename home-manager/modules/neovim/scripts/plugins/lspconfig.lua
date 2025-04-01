7c079b13bb7eb86ed75b299915fa028b06f7e96d
Ivar Scholten <ivar.scholten@protonmail.com>
2025-04-01 18:35:36 +0200

    nvim: ask rust-analyzer to ignore more nix symlinks

--- home-manager/modules/neovim/scripts/plugins/lspconfig.lua
+++ home-manager/modules/neovim/scripts/plugins/lspconfig.lua
@@ -40,14 +40,23 @@ local function loadLanguageServers()
         rust_analyzer = {
             settings = {
                 ["rust-analyzer"] = {
+                    files = {
+                        exclude = {
+                            -- Ignore all files/directories that contain symlinks to the nix store, as that seemingly causes rust-analyzer to scan the entire store:
+                            -- https://github.com/rust-lang/rust-analyzer/issues/14734#issuecomment-2373988391
+                            ".direnv",
+                            "result",
+                            "result-dev",
+                            "result-man",
+                            "result-out",
+                        }
+                    },
+
                     -- Show diagnostics from `cargo clippy` instead of `cargo check`. The former is a bit stricter.
                     checkOnSave = { command = "clippy" },
+
                     -- Don't show diagnostics for inactive cfg directives.
                     diagnostics = { disabled = { "inactive-code" } },
-                    -- Ignore all files from direnv, as those contain symlinked directories to the nix store,
-                    -- which seemingly causes rust-analyzer to scan the entire store:
-                    -- https://github.com/rust-lang/rust-analyzer/issues/14734#issuecomment-2373988391
-                    files = { excludeDirs = { ".direnv" } },
                 }
             }
         },
