d8075753d449854630434ce9d488d1f67340ad66
Ivar Scholten <ivar.scholten@protonmail.com>
2025-05-11 19:10:13 +0200

    monitor-layout: add second monitor

--- home-manager/modules/linux/i3-sway/monitor-layouts/pc.nix
+++ home-manager/modules/linux/i3-sway/monitor-layouts/pc.nix
@@ -13,6 +13,6 @@
     };
 
     Install.WantedBy = [ "graphical-session.target" ];
-    Service.ExecStart = "${lib.getExe pkgs.xorg.xrandr} --output DP-0 --primary --mode 3440x1440 --pos 1280x0 --rotate normal";
+    Service.ExecStart = "${lib.getExe pkgs.xorg.xrandr} --output DVI-D-0 --off --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off";
   };
 }
