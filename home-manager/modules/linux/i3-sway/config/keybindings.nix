6aeb7e6906848674aa499b099c6e0736b974ea2b
Ivar Scholten <ivar.scholten@protonmail.com>
2025-09-22 13:19:03 +0200

    i3/sway: proper tab support

--- home-manager/modules/linux/i3-sway/config/keybindings.nix
+++ home-manager/modules/linux/i3-sway/config/keybindings.nix
@@ -7,41 +7,54 @@ workspaces:
 }:
 
 let
+  # Switch to the next/previous tab in a tabbed layout. Adapted from https://github.com/swaywm/sway/issues/8537#issuecomment-2874633061, thanks!
+  switchTab = dir: msgCmd:
+    let
+      offset = if dir == "left" then "-1" else "1";
+      wrap = if dir == "left" then "-1" else "0";
+    in
+    pkgs.writeShellScript "sway-switch-tab-${dir}" ''
+      target="$(${msgCmd} -t get_tree | ${lib.getExe pkgs.jq} -r '
+          first(.nodes, .floating_nodes | recurse(.[] | .nodes, .floating_nodes) | select(any(.focused)))
+          | length as $len | . as $nodes | map(.focused) | index(true) as $idx
+          | $nodes[if $idx + ${offset} >= $len then ${wrap} else $idx + ${offset} end].id')"
+      [[ "''${target:-"null"}" != "null" ]] && ${msgCmd} "[con_id=$target] focus"
+    '';
+
+  # Focus the window in the given direction. If the currently focused window is a tab, jump over other windows in the same tab container.
+  focusDirection = dir: msgCmd: pkgs.writeShellScript "sway-focus-parent-if-tabbed-${dir}" ''
+    layout="$(${msgCmd} -t get_tree | ${lib.getExe pkgs.jq} -r 'recurse(.nodes[], .floating_nodes[]) | select(.nodes[], .floating_nodes[] | .focused).layout')"
+    if [[ ''${layout:-"null"} == "tabbed" ]]; then
+      ${msgCmd} "focus parent; focus ${dir}; focus child"
+    else
+      ${msgCmd} "focus ${dir}"
+    fi
+  '';
+
   mkKeybindings = modifier: msgCmd: {
-    "${modifier}+d" = "exec --no-startup-id ${pkgs.dmenu-configured}/bin/dmenu_run";
-
-    # General programs
-    "${modifier}+Return" = "exec --no-startup-id ${config.home.sessionVariables.TERMINAL}";
-    "${modifier}+Shift+w" = "exec \"${msgCmd} 'workspace ${workspaces.ws2}; exec ${lib.getExe pkgs.qutebrowser} --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=2'\"";
-    "${modifier}+Shift+d" = "exec ${pkgs.discord-with-openasar}/bin/Discord";
-    "${modifier}+Shift+Ctrl+d" = "exec pkill Discord && pkill Discord";
-    "${modifier}+Shift+s" = "exec \"${msgCmd} 'workspace ${workspaces.ws4}; exec ${lib.getExe pkgs.tidal-hifi}'\"";
-    "${modifier}+Shift+t" = "exec ${pkgs.transmission_4-gtk}/bin/transmission-gtk";
-
     # Navigation
-    "${modifier}+h" = "focus left";
-    "${modifier}+l" = "focus right";
-    "${modifier}+k" = "focus up";
-    "${modifier}+j" = "focus down";
     "${modifier}+Shift+h" = "move left";
     "${modifier}+Shift+l" = "move right";
     "${modifier}+Shift+k" = "move up";
     "${modifier}+Shift+j" = "move down";
+
+    "${modifier}+h" = "exec ${focusDirection "left" msgCmd}";
+    "${modifier}+l" = "exec ${focusDirection "right" msgCmd}";
+    "${modifier}+k" = "exec ${focusDirection "up" msgCmd}";
+    "${modifier}+j" = "exec ${focusDirection "down" msgCmd}";
+
+    "${modifier}+bracketleft" = "exec ${switchTab "left" msgCmd}";
+    "${modifier}+bracketright" = "exec ${switchTab "right" msgCmd}";
+
     "${modifier}+Ctrl+h" = "resize shrink width 10 px or 10 ppt";
     "${modifier}+Ctrl+l" = "resize grow width 10 px or 10 ppt";
     "${modifier}+Ctrl+k" = "resize shrink height 10 px or 10 ppt";
     "${modifier}+Ctrl+j" = "resize grow height 10 px or 10 ppt";
-    "${modifier}+Shift+Ctrl+h" = "split h";
+
+    # Workspace management
     "${modifier}+n" = "move workspace to output right";
-    "${modifier}+v" = "split v";
-    "${modifier}+s" = "layout stacking";
-    "${modifier}+w" = "layout tabbed";
-    "${modifier}+e" = "layout toggle split";
-    "${modifier}+f" = "fullscreen toggle";
-    "${modifier}+Shift+space" = "floating toggle";
-    "${modifier}+space" = "focus mode_toggle";
-    "${modifier}+a" = "focus parent";
-    "${modifier}+Shift+q" = "kill";
+    "${modifier}+Shift+n" = "move workspace to output left";
+
     "${modifier}+1" = "workspace ${workspaces.ws1}";
     "${modifier}+2" = "workspace ${workspaces.ws2}";
     "${modifier}+3" = "workspace ${workspaces.ws3}";
@@ -52,6 +65,7 @@ let
     "${modifier}+8" = "workspace ${workspaces.ws8}";
     "${modifier}+9" = "workspace ${workspaces.ws9}";
     "${modifier}+0" = "workspace ${workspaces.ws10}";
+
     "${modifier}+Shift+1" = "move container to workspace ${workspaces.ws1}";
     "${modifier}+Shift+2" = "move container to workspace ${workspaces.ws2}";
     "${modifier}+Shift+3" = "move container to workspace ${workspaces.ws3}";
@@ -63,11 +77,20 @@ let
     "${modifier}+Shift+9" = "move container to workspace ${workspaces.ws9}";
     "${modifier}+Shift+0" = "move container to workspace ${workspaces.ws10}";
 
+    # Layout management
+    "${modifier}+Shift+Ctrl+h" = "split h";
+    "${modifier}+v" = "split v";
+    "${modifier}+w" = "layout tabbed";
+    "${modifier}+e" = "layout toggle split";
+    "${modifier}+f" = "fullscreen toggle";
+    "${modifier}+Shift+space" = "floating toggle";
+    "${modifier}+space" = "focus mode_toggle";
+    "${modifier}+a" = "focus parent";
+
     # Volume control
     "XF86AudioRaiseVolume" = "exec ${lib.getExe pkgs.pamixer} --increase 5";
     "XF86AudioLowerVolume" = "exec ${lib.getExe pkgs.pamixer} --decrease 5";
-    # Mute both the input and output devices
-    "XF86AudioMute" = "exec ${lib.getExe pkgs.pamixer} --toggle-mute && ${lib.getExe pkgs.pamixer} --default-source --toggle-mute";
+    "XF86AudioMute" = "exec ${lib.getExe pkgs.pamixer} --toggle-mute && ${lib.getExe pkgs.pamixer} --default-source --toggle-mute"; # Mute both the input and output devices
 
     # Media player control
     "XF86AudioPause" = "exec ${lib.getExe pkgs.playerctl} play-pause";
@@ -78,6 +101,15 @@ let
     # Brightness control
     "XF86MonBrightnessUp" = "exec ${lib.getExe pkgs.brightnessctl} set 5%+";
     "XF86MonBrightnessDown" = "exec ${lib.getExe pkgs.brightnessctl} set 5%-";
+
+    # Programs
+    "${modifier}+Shift+q" = "kill";
+    "${modifier}+d" = "exec --no-startup-id ${pkgs.dmenu-configured}/bin/dmenu_run";
+    "${modifier}+Return" = "exec --no-startup-id ${config.home.sessionVariables.TERMINAL}";
+    "${modifier}+Shift+w" = "exec \"${msgCmd} 'workspace ${workspaces.ws2}; exec ${lib.getExe pkgs.qutebrowser} --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=2'\"";
+    "${modifier}+Shift+d" = "exec ${pkgs.discord-with-openasar}/bin/Discord";
+    "${modifier}+Shift+Ctrl+d" = "exec pkill Discord && pkill Discord";
+    "${modifier}+Shift+s" = "exec \"${msgCmd} 'workspace ${workspaces.ws4}; exec ${lib.getExe pkgs.tidal-hifi}'\"";
   };
 
   mkExitBinding = msgCmd: name:
