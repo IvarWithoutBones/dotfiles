59f2f732adb089c3e7b2c79ae775faab88c05484
Ivar Scholten <ivar.scholten@protonmail.com>
2026-01-10 00:19:26 +0100

    jellyfin: add flaresolverr

--- modules/linux/jellyfin.nix
+++ modules/linux/jellyfin.nix
@@ -1,33 +1,41 @@
 { ... }:
 
 {
-  # Media player, hosts a web interface
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-  };
+  services = {
+    # Media player, hosts a web interface
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Request manager for jellyfin, unifying series and movies
-  services.jellyseerr = {
-    enable = true;
-    openFirewall = true;
-  };
+    # Request manager for jellyfin, unifying series and movies
+    jellyseerr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Series manager, interfaces with jellyseerr
-  services.sonarr = {
-    enable = true;
-    openFirewall = true;
-  };
+    # Series manager, interfaces with jellyseerr
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Movie manager, interfaces with jellyseerr
-  services.radarr = {
-    enable = true;
-    openFirewall = true;
-  };
+    # Movie manager, interfaces with jellyseerr
+    radarr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Provides the indexers for sonarr and radarr
-  services.prowlarr = {
-    enable = true;
-    openFirewall = true;
+    # Provides the indexers for sonarr and radarr
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    # Bypasses cloudfare for indexers
+    flaresolverr = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
