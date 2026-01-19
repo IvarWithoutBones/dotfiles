685e8127ee7db2087aab517001848385d7b6583d
Ivar Scholten <ivar.scholten@protonmail.com>
2026-01-19 21:22:30 +0100

    udev: add picoscope

--- modules/linux/udev.nix
+++ modules/linux/udev.nix
@@ -26,10 +26,13 @@
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="8089", ATTRS{idProduct}=="0007", MODE="0660", GROUP="plugdev", TAG+="uaccess", TAG+="udev-acl"
 
       # 8BitDo Ultimate 2 Dongle/2.4Ghz receiver (DInput)
-      KERNEL=="hidraw*", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="6012", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="6012", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
 
       # 8BitDo Ultimate 2 Bluetooth (DInput)
-      KERNEL=="hidraw*", KERNELS=="*2DC8:6012*", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", KERNELS=="*2DC8:6012*", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+
+      # Picoscope 2000A Oscilloscope
+      SUBSYSTEM=="usb", ATTR{idVendor}=="0ce9", ATTR{idProduct}=="1016", MODE="0660", GROUP="plugdev", TAG+="uaccess", TAG+="udev-acl"
     '';
   };
 }
