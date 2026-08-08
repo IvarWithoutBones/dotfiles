97cf829e69efe777b3f363c63d7b0d62cba407a4
Ivar Scholten <ivar.scholten@protonmail.com>
2026-08-09 01:39:39 +0200

    mpv: add hardware decoding options

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.mpv = {
@@ -8,6 +8,13 @@
       profile = "gpu-hq";
       ytdl-format = "bestvideo+bestaudio";
       save-position-on-quit = true;
+    }
+    // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+      # Use the GPU for video decoding and rendering on Linux, preferably with Vulkan.
+      gpu-context = "waylandvk,wayland,x11vk,x11egl,displayvk,drm";
+      gpu-api = "vulkan,opengl";
+      vo = "gpu-next,"; # Trailing comma makes mpv try options not in the list as a fallback.
+      hwdec = "nvdec,vaapi,drm,vulkan,auto,auto-copy";
     };
   };
 }
